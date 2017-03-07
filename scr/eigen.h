db3bee9d09d1c767279df3058c6825895123114b
sarahGK <xin.kelly@hotmail.com>
2017-03-07 11:56:17 -0800

    Update eigen.h

--- scr/eigen.h
+++ scr/eigen.h
@@ -9,5 +9,4 @@
 using namespace std;
 
 int eejcb(double * a,int n,double *v, double eps,int jt);
-//void eastrq(double *a,int n,double *q,double *b,double *c);
 void SortEigen(double mu[3],double a[3][3]);
